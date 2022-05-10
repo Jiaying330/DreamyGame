58ba719b1fbf2f23d011504c116daa3306432d24
Jiaying330 <jiaying.hou330@gmail.com>
2022-05-10 11:29:27 +0800

    updates
    * Rotating platform
    * Player Death Trigger
    * Pendulum

---
+++ dreamy/Assets/CharacterEditorPackage/Code/Game/DemoObjects/Rotator1.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator1 : MonoBehaviour
+{
+    Rigidbody rb;
+    public float moveSpeed;
+    public float leftAngle;
+    public float rightAngle;
+
+    bool movingClockwise;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        movingClockwise = true;
+
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    public void ChangeMoveDir()
+    {
+        if(transform.rotation.z > rightAngle)
+        {
+            movingClockwise = false;
+            // rightAngle = Random.Range(0.1f, 0.8f);
+            Debug.Log("movingClockwise = " + movingClockwise);
+        }
+        if(transform.rotation.z < leftAngle)
+        {
+            movingClockwise = true;
+            // leftAngle = Random.Range(-0.1f, -0.8f);
+            Debug.Log("movingClockwise = " + movingClockwise);
+            // Debug.Log(rightAngle);
+        }
+    }
+
+    public void Move()
+    {
+        ChangeMoveDir();
+        // Debug.Log("transform.rotation.z = " + transform.rotation.z );
+        if(movingClockwise)
+        {
+            rb.transform.Rotate(Vector3.up, Time.fixedDeltaTime * moveSpeed);
+            // rb.angularVelocity = moveSpeed;
+        }
+        else
+        {
+            rb.transform.Rotate(Vector3.up, Time.fixedDeltaTime * -moveSpeed);
+        }
+    }
+}
--- dreamy/Assets/CharacterEditorPackage/Code/Game/LevelManagement/DeathTrigger.cs
+++ dreamy/Assets/CharacterEditorPackage/Code/Game/LevelManagement/DeathTrigger.cs
@@ -8,19 +8,28 @@ public class DeathTrigger : MonoBehaviour {
 
     void OnTriggerEnter(Collider a_Collider)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        // ControlledCapsuleCollider controlledCapsuleCollider = a_Collider.GetComponent<ControlledCapsuleCollider>();
-        // if (controlledCapsuleCollider != null)
-        // {
-        //     //Prevent death state to be used if the collider is no-clipping
-        //     if (controlledCapsuleCollider.AreCollisionsActive())
-        //     { 
-        //         Debug.Log("Death triggered by: " + transform.name);
-        //         if (InSceneLevelSwitcher.Get())
-        //         {
-        //             InSceneLevelSwitcher.Get().Respawn();
-        //         }
-        //     }
-        // }
+        if (a_Collider.tag == "Death") {
+            Debug.Log("Death triggered by: " + transform.name);
+                // if (InSceneLevelSwitcher.Get())
+                // {
+                //     InSceneLevelSwitcher.Get().Respawn();
+                // }
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        ControlledCapsuleCollider controlledCapsuleCollider = a_Collider.GetComponent<ControlledCapsuleCollider>();
+        if (controlledCapsuleCollider != null)
+        {
+            // Debug.Log("a_Collider = " + a_Collider);
+            //Prevent death state to be used if the collider is no-clipping
+            if (controlledCapsuleCollider.AreCollisionsActive())
+            { 
+                Debug.Log("Death triggered by: " + transform.name);
+                // if (InSceneLevelSwitcher.Get())
+                // {
+                //     InSceneLevelSwitcher.Get().Respawn();
+                // }
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
     }
 }
